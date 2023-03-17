c9df494ddd0cd34c6b5c66eb5e9f756d99ac7b0a
Rakshitha.D <rakshithagowda.d@gmail.com>
2023-03-17 19:39:41 +0530

    props
    used props in the components instead of constant variables

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,13 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-    const expenseDate = new Date(2023,2,28);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67;
-
+function ExpenseItem(props) {
+    
     return (
         <div className='expense-item'>
-            <div>March 13th 2023</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>$294.67</div>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>{props.amount}</div>
             </div>
         </div>
     );
