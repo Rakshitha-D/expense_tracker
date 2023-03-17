7d3d5b67ca264a8eb9b99eaf140d0526a86183ca
Rakshitha.D <rakshithagowda.d@gmail.com>
2023-03-18 00:21:56 +0530

    new ExpenseDate Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,11 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date}></ExpenseDate>{" "}
+      {/* can be written ass <ExpenseDate/> if there is no content between the opening and closing tag*/}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
