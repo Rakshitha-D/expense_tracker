e0246f3e1408a6c4a8ca855f8b303ddc96d681d9
Rakshitha.D <rakshithagowda.d@gmail.com>
2023-03-24 11:27:36 +0530

    new Card component
    use Card component as
    wrapper component and use props.children to use Card as wrapper component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,17 @@
+import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>{" "}
       {/* can be written ass <ExpenseDate/> if there is no content between the opening and closing tag*/}
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
