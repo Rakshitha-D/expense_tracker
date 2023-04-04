fe74ca801a79b1cabc1025444c4b95857ec5abe7
Rakshitha.D <rakshithagowda.d@gmail.com>
2023-04-04 19:43:58 +0530

    useState Hooks
    imported useState hooks from react to change the state title when button is clicked

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,23 @@
+import React, { useState } from "react";
+
 import Card from "./Card";
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const onClickHandler = () => {
+    setTitle("Updated!");
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>{" "}
       {/* can be written ass <ExpenseDate/> if there is no content between the opening and closing tag*/}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
+        <button onClick={onClickHandler}>Rename title</button>
       </div>
     </Card>
   );
